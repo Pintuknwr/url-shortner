f092d02c925818d804c9e55165279fea858f4f94
pintuknwr <pintuk.ug22.cs@nitp.ac.in>
2024-08-13 19:28:15 +0530

    third

--- controllers/url.js
+++ controllers/url.js
@@ -1,34 +1,33 @@
-const shortid=require("shortid");
-const URL=require("../models/url");
-async function handleGenerateURL(req,res){
-    const body=req.body;
-    if(!body.url) return res.status(400).json({error:"URL is required"})
-const shortID = shortid();
-await URL.create({
+const shortid = require("shortid");
+const URL = require("../models/url");
+
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
+  const shortID = shortid();
+
+  await URL.create({
     shortId: shortID,
     redirectURL: body.url,
-    visitHistory:[],
+    visitHistory: [],
+    createdBy: req.user._id,
+  });
 
-
-
-}) ;
-return res.render("home",{
+  return res.render("home", {
     id: shortID,
-
-})
+  });
 }
 
-
-
-async function handleGetAnalytics(req,res){
-    const shortId=req.params.shortId;
-    const result= await URL.findOne({shortId});
-    return res.json({titalClicks:result.visitHistory.length,
-     analytics:result.visitHistory,
-    })
- 
- }
-module.exports={
-    handleGenerateURL,
-    handleGetAnalytics,
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
 }
+
+module.exports = {
+  handleGenerateNewShortURL,
+  handleGetAnalytics,
+};
